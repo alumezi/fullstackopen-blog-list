014a53c3f04062b3d71216aa979124fc34287c31
alumezi <arbnorlumezi@gmail.com>
2020-10-04 22:32:28 +0200

    5.1 Login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Login from './components/Login'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
+
+  const handleLogin = async event => {
+    event.preventDefault()
+    const user = await loginService.login(username, password)
+    setUser(user)
+  }
+
+  if (user === null) {
+    return <Login handleLogin={handleLogin} username={username} password={password} setUsername={setUsername} setPassword={setPassword} />
+  }
 
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
