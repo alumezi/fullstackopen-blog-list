0aaa3ff67e35f17e55b57df7b2fd856e6e7525d4
alumezi <arbnorlumezi@gmail.com>
2020-10-17 18:53:23 +0200

    5.7* Blog list frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,23 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState()
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return <div style={blogStyle}>
+    {blog.title} {blog.author} <button onClick={() => setShowDetails(!showDetails)}>{showDetails ? 'hide' : 'view'}</button>
+    <div style={{ display: showDetails ? '' : 'none' }}>
+      <div>{blog.url}</div>
+      <div>{blog.likes} <button onClick={() => console.log("Liked")}>like</button></div>
+      <div>{blog.author}</div>
+    </div>
   </div>
-)
+}
 
 export default Blog
