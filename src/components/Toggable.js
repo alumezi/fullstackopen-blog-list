ab54283bcbdd185a8a2f951f8490880334a0e4db
alumezi <arbnorlumezi@gmail.com>
2020-10-21 18:30:24 +0200

    5.11

--- src/components/Toggable.js
+++ src/components/Toggable.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Toggable = ({ visible, toggle, buttonLabel, children }) => {
     const style = {
@@ -12,6 +13,13 @@ const Toggable = ({ visible, toggle, buttonLabel, children }) => {
             <button onClick={() => toggle(false)}>cancel</button>
         </div>
     </>
+}
+
+Toggable.propTypes = {
+    visible : PropTypes.bool.isRequired,
+    toggle : PropTypes.func.isRequired,
+    buttonLabel : PropTypes.string.isRequired,
+    children : PropTypes.node.isRequired,
 }
 
 export default Toggable
